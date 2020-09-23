14efc539644d2699d241a9380041dec7da57954f
Lorenzo Pappalardo <lorenzopappalardo7@gmail.com>
2020-09-23 08:53:02 +0200

    Updates

---
+++ precache-manifest.2b185372ffb347772945cc92b0e50da0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "007032359deca1b42bd2f28c98063a91",
+    "url": "/unict-telegram-channels-by-me/index.html"
+  },
+  {
+    "revision": "53af9e9696942ecf02ab",
+    "url": "/unict-telegram-channels-by-me/static/css/main.acfd1ce0.chunk.css"
+  },
+  {
+    "revision": "90702d3030ef52a4a501",
+    "url": "/unict-telegram-channels-by-me/static/js/2.cc0e3b15.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/unict-telegram-channels-by-me/static/js/2.cc0e3b15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53af9e9696942ecf02ab",
+    "url": "/unict-telegram-channels-by-me/static/js/main.1a0bcfa9.chunk.js"
+  },
+  {
+    "revision": "8534c318cf4dd4416db4",
+    "url": "/unict-telegram-channels-by-me/static/js/runtime-main.84d9977a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/unict-telegram-channels-by-me/static/media/logo.5d5d9eef.svg"
+  }
+]);
